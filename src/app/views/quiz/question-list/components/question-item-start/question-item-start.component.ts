57cf63cfcbf6365155fc574c51f6a3d3bd689ae0
Matej Hočevar <info@matox.si>
2018-04-25 19:49:09 +0200

    style for empty question item updated

--- src/app/views/quiz/question-list/components/question-item-start/question-item-start.component.ts
+++ src/app/views/quiz/question-list/components/question-item-start/question-item-start.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-question-item-start',
   templateUrl: './question-item-start.component.html',
-  styleUrls: ['./question-item-start.component.css']
+  styleUrls: ['./question-item-start.component.styl']
 })
 export class QuestionItemStartComponent implements OnInit {
 
