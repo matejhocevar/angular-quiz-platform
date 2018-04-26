e3982eb0a2536231dd1127e1a2ab3c700a40e049
Matej Hočevar <info@matox.si>
2018-04-26 07:29:48 +0200

    enivironment variables updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,16 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: '',
-    authDomain: ''
+    apiKey: '<your-api-key>',
+    authDomain: '<your-auth-domain>',
+    databaseURL: '<your-database-url>',
+    projectId: '<your-project-id>',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-messaging-sender-id>'
+  },
+  quiz: {
+    title: '',
+    description: '',
+    imagePath: ''
   }
 };
