4289d3716620ac0726d3df16f9331e7a35e0dc9b
Matej Hočevar <info@matox.si>
2018-04-28 14:15:10 +0200

    answers form added to question card

--- src/app/views/quiz/question/question.component.ts
+++ src/app/views/quiz/question/question.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Params, Router} from '@angular/router';
 import {QuizService} from '../quiz.service';
 import {Question} from '../../../models/quiz/question.model';
-import {Title} from '@angular/platform-browser';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-question',
@@ -12,10 +12,12 @@ import {Title} from '@angular/platform-browser';
 export class QuestionComponent implements OnInit {
   question: Question;
   id: number;
+  questionForm: FormGroup;
 
   constructor(private quizService: QuizService,
               private route: ActivatedRoute,
-              private router: Router) { }
+              private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
     this.id = +this.route.snapshot.params['id'];
@@ -25,7 +27,23 @@ export class QuestionComponent implements OnInit {
       .subscribe((params: Params) => {
         this.id = +params['id'];
         this.question = this.quizService.getQuestionByOrder(this.id);
+        this.initForm();
       });
+  }
+
+  private initForm() {
+    this.questionForm = this.formBuilder.group({
+      answers: this.formBuilder.array([])
+    });
+
+    for (const a of this.question.answers) {
+      const answerArray = (this.questionForm.get('answers')) as FormArray;
+      answerArray.push(this.formBuilder.group({
+        text: a.text,
+        isCorrect: a.isCorrect,
+        answer: a.answer
+      }));
+    }
   }
 
   numOfCorrectAnswers() {
